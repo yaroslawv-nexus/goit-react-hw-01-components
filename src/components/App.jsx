8a72fb17dac06b388537d8b959ab440f63b4a1b5
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-07-31 21:15:19 +0300

    compProfile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
+import { Profile } from "./Profile/Profile";
+import user from '../json/user.json';
+
 export const App = () => {
   return (
-    <div>
-      React homework template
-    </div>
+    <Profile 
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats} />
   );
 };
